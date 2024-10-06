73684d4a04056ca8685c66ea8d482f4b97674d12
Gabriel Felipe <belebraz16@gmail.com>
2024-10-06 20:10:35 -0300

    feat: route Today

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {TodayComponent} from "./pages/today/today.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', component: TodayComponent },
+];
