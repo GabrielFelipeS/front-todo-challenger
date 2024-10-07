16996192be06e411f2ffd8d6bc11ae7f7ee88f1e
Gabriel Felipe <belebraz16@gmail.com>
2024-10-07 14:39:20 -0300

    refactor: aside component

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ThemeSwitcherComponent} from "../theme-switcher/theme-switcher.component";
+import {AsideMenuOptionComponent} from "./aside-menu-option/aside-menu-option.component";
+import {Task} from "../../models/Task";
+import {TaskService} from "../../services/task.service";
+import * as TaskFilters from '../../utils/task-filters';
 
 @Component({
   selector: 'app-aside',
   standalone: true,
   imports: [
-    ThemeSwitcherComponent
+    ThemeSwitcherComponent,
+    AsideMenuOptionComponent
   ],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.scss'
 })
-export class AsideComponent {
+export class AsideComponent implements OnInit{
+  tasks: Task[] = [];
+  taskFilters = TaskFilters;
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    })
+  }
 
 }
