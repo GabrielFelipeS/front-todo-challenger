919b89ecabb63f9652ccf789e1052969f71080aa
Gabriel Felipe <belebraz16@gmail.com>
2024-10-11 15:59:30 -0300

    feat: task-list component

--- src/app/components/main/task-list/task-list.component.ts
+++ src/app/components/main/task-list/task-list.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Task} from "../../../models/Task";
+import {CheckButtonComponent} from "./check-button/check-button.component";
+import {TaskInfoComponent} from "./task-info/task-info.component";
+import {PLANNED_TASKS_FILTER_PENDING_OR_IN_PROGRESS} from "../../../utils/task-filters";
 
 @Component({
   selector: 'task-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CheckButtonComponent,
+    TaskInfoComponent
+  ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit, OnChanges{
+  @Input() filteredTasks!: Task[];
+  notCompletedTasks: Task[] = [];
+
+  ngOnInit() {
+    this.filterTask();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    setTimeout(() => {
+      this.filterTask();
+    }, 2250);
+  }
+
+  filterTask() {
+    this.notCompletedTasks = this.filteredTasks.filter(PLANNED_TASKS_FILTER_PENDING_OR_IN_PROGRESS)
+
+  }
+
+
 
 }
