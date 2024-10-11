5e36a60e1c830850f93b044367d879a7102d8aa2
Gabriel Felipe <belebraz16@gmail.com>
2024-10-11 10:00:54 -0300

    feat: main component in today.component.html

--- src/app/pages/today/today.component.ts
+++ src/app/pages/today/today.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {AsideComponent} from "../../components/aside/aside.component";
+import {MainComponent} from "../../components/main/main.component";
 
 @Component({
   selector: 'app-today',
   standalone: true,
   imports: [
-    AsideComponent
+    AsideComponent,
+    MainComponent
   ],
   templateUrl: './today.component.html',
   styleUrl: './today.component.scss'
